c0d2730cde24ccbf6d1ba24b5af0887b6e80b9df
Stephen Poter <me@stevepotter.me>
2017-08-07 22:18:08 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Showcasing the UXCam React Native ScrollView click bug
+
+Affects iOS only and Android project hasn't been set up.
+
---
+++ index.ios.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, View, ScrollView, TouchableOpacity, Text } from 'react-native';
+//import UXCam from 'react-native-ux-cam';
+
+export default class UXCamScrollView extends Component {
+
+  componentDidMount() {
+//    UXCam.startWithKey('YOUR KEY')
+  }
+
+  render() {
+    return (
+      <ScrollView
+        keyboardShouldPersistTaps="always"
+        keyboardDismissMode="on-drag"
+      >
+        <TouchableOpacity onPress={() => {console.log("hi")}}>
+          <Text style={{
+            width: 50,
+            height: 50,
+            backgroundColor: '#aaaaaa',
+            margin: 20
+          }}>Hsi</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('UXCamScrollView', () => UXCamScrollView);
